5dbfd6ee8efabf8acc98d76e3139457aac6eb4ed
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-18 09:40:20 -0300

    Estrutura básica finalizada.

--- Cliente.cs
+++ Cliente.cs
@@ -2,6 +2,18 @@ namespace ExiFood
 {
     public class Cliente
     {
-        
+        public string Nome { get; set; }
+        public string EnderecoAtual { get; set; }
+
+        public Cliente(string _nome){
+            this.Nome = _nome;
+        }
+        public Cliente(string _nome, string _enderecoatual){
+            this.Nome = _nome;
+            this.EnderecoAtual = _enderecoatual;
+        }
+        public string MostrarDados(){
+            return $"{Nome} (você) se localiza em {EnderecoAtual}.";
+        }
     }
 }
--- Restaurante.cs
+++ Restaurante.cs
@@ -2,6 +2,19 @@ namespace ExiFood
 {
     public class Restaurante
     {
-        
+        public string NomeFantasia { get; set; }
+        public string Endereco { get; set; }
+
+        public Restaurante(string _endereco){
+            this.Endereco = _endereco;
+        }
+        public Restaurante(string _nomefantasia, string _endereco){
+            this.NomeFantasia = _nomefantasia;
+            this.Endereco = _endereco;
+        }
+        public string MostrarDados()
+        {
+            return $"O restaurante {NomeFantasia} se localiza em {Endereco}.";
+        }
     }
 }
